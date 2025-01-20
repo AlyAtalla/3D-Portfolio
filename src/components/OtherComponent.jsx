dfe8a700257de6ae0457b5a67a29731d94205623
Aly Atalla <aliatalla93@GMAIL.com>
2025-01-20 13:22:40 +0200

    Replace react-tilt with react-parallax-tilt in package.json and About component; add OtherComponent using react-parallax-tilt

---
+++ src/components/OtherComponent.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// ...existing code...
+import Tilt from "react-parallax-tilt";
+// ...existing code...
+
+const OtherComponent = () => {
+  return (
+    <div>
+      {/* Example usage of Tilt */}
+      <Tilt>
+        <div>
+          {/* Content inside the tilt effect */}
+        </div>
+      </Tilt>
+      {/* ...existing code... */}
+    </div>
+  );
+};
+
+export default OtherComponent;
