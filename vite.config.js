7afb5280a1d5fcb64c48a8723b204c9a26a802c0
Aly Atalla <aliatalla93@GMAIL.com>
2025-01-21 13:26:51 +0200

    Add GLTF file support and optimize build configuration in Vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.gltf'], // Add support for GLTF files
+  optimizeDeps: {
+    include: ['three', '@react-three/fiber', '@react-three/drei']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('desktop_pc')) {
+            return 'model-chunk';
+          }
+        }
+      }
+    },
+    // Adjust chunk size warning limit
+    chunkSizeWarningLimit: 1000
+  }
 })
