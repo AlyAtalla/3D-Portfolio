ab5442f9e90062a2f03e865b6341a29010854880
Aly Atalla <aliatalla93@GMAIL.com>
2025-01-21 20:05:43 +0200

    Update Tech component to display a subset of technologies for improved focus

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
@@ -7,7 +6,7 @@ import { technologies } from "../constants";
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
+      {technologies.slice(7).map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
